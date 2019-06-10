3105b4a934ce61f5f65e2a9ed433caffa9e17ede
Isabella Tea <tea.isabella@gmail.com>
2019-06-10 13:47:54 -0700

    completed bank activity tasks

--- javaScript/labs/Banking_101/script.js
+++ javaScript/labs/Banking_101/script.js
@@ -14,23 +14,60 @@
 
 let customer_name;
 let balance;
+let logged_in = false;
+let custome_password;
 
-function openAccount(name){
-  balance = 0;
+function openAccount(name, startingBalance = 0, password){
+  balance = startingBalance;
   // Set the value for customer_name equal to name below
+  customer_name = name;
+  customer_password = password;
 
-  return //write the statment you need to return here
+  //write the statment you need to return here
+  return `${name} has opened a new account with a balance of $${balance}`
+
 }
 
 function deposit(value){
+  if (logged_in === false) {
+    return 'User must be logged in';
+  } else {
+      balance += value;
+      return `${customer_name} has deposited ${value}. ${customer_name}'s total balance is $${balance}`;
+  }
   // update the value of balance
   //return the correct statement
 }
 
-function withdraw(/*argument here*/){
+function withdraw(withdrawAmount){
+  if (logged_in === false) {
+    return 'User must be logged in';
+  } else {
   //update the value of balance
   //return the correct statement
+    if (withdrawAmount > balance) {
+      return `Sorry ${customer_name}, you do not have enough money in your account. You are short by $${withdrawAmount-balance} dollars. `
+    } else {
+      balance -= withdrawAmount;
+      return `${customer_name} has withdrawn ${withdrawAmount}. ${customer_name} has ${balance} remaining.`
+    }
+  }
+}
+
+function login(inputName, inputPassword) {
+  if (inputName === customer_name && inputPassword === customer_password) {
+    logged_in = true;
+    return `${customer_name} has logged in.`
+  } else {
+    logged_in = false;
+    return 'Incorrrect login';
+  }
+}
+
+function logout() {
+  logged_in = false;
+  return `${customer_name} has logged out.`
 }
 
 // Write your script below
-console.log("script is running...");
+console.log("script is running...?");
